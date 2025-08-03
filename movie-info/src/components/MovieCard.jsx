5872039c36901301c4ec180c22707c5795e337b3
hannalee7222 <hannah.lee7222@gmail.com>
2025-08-03 23:51:58 +0900

    ⛏️영화카드 클릭안되는 에러 수정, 코드 확장자명 수정

--- movie-info/src/components/MovieCard.jsx
+++ movie-info/src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function MovieCard({
-  poster_path,
-  title,
-  vote_average,
-  id,
-  onClick,
-}) {
+export default function MovieCard({ poster_path, title, vote_average, id }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/details/${id}`);
+  };
+
   return (
     <div
       className="w-full max-w-[180px] mx-auto flex flex-col items-center cursor-pointer text-center"
-      onClick={() => onClick(id)}
+      onClick={handleClick}
     >
       <img
         src={`https://image.tmdb.org/t/p/w300${poster_path}`}
